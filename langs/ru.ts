c3b96062e1fbf2a5c02b6b9f4449a90e1ee9a30d
olegrok <babinoleg@mail.ru>
2016-05-20 20:05:55 +0300

    add lang files

---
+++ langs/ru.ts
@@ -0,0 +1,246 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>AddFriend</name>
+    <message>
+        <location filename="addfriend.ui" line="26"/>
+        <source>Add new friends</source>
+        <translation>Добавить нового друга</translation>
+    </message>
+    <message>
+        <location filename="addfriend.ui" line="41"/>
+        <source>Login</source>
+        <translation>Логин</translation>
+    </message>
+    <message>
+        <location filename="addfriend.ui" line="60"/>
+        <source>Add</source>
+        <translation>Добавить</translation>
+    </message>
+    <message>
+        <location filename="addfriend.ui" line="70"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <location filename="addfriend.ui" line="89"/>
+        <source>Enter your friend&apos;s nickname</source>
+        <translation>Введите логин контакта</translation>
+    </message>
+    <message>
+        <location filename="addfriend.cpp" line="29"/>
+        <source>Just your friend</source>
+        <translation>Уже ваш друг</translation>
+    </message>
+    <message>
+        <location filename="addfriend.cpp" line="36"/>
+        <source>Not found</source>
+        <translation>Не найдено</translation>
+    </message>
+    <message>
+        <location filename="addfriend.cpp" line="38"/>
+        <source>Unknown error</source>
+        <translation>Неизвестная ошибка</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="26"/>
+        <source>Chat</source>
+        <translation>Чат</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="125"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="151"/>
+        <source>Send</source>
+        <translation>Отправить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="101"/>
+        <location filename="mainwindow.ui" line="214"/>
+        <source>Options</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="91"/>
+        <source>Delete contact</source>
+        <translation>Удалить контакт</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="173"/>
+        <source>Find login</source>
+        <translation>Поиск логина</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="186"/>
+        <source>Find in messages</source>
+        <translation>Поиск среди сообщений</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="199"/>
+        <location filename="mainwindow.cpp" line="205"/>
+        <location filename="mainwindow.cpp" line="210"/>
+        <source>Find</source>
+        <translation>Найти</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="222"/>
+        <source>About</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="232"/>
+        <source>&amp;Exit</source>
+        <translation>&amp;Выход</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="237"/>
+        <source>&amp;Options</source>
+        <translation>&amp;Настройки</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="81"/>
+        <location filename="mainwindow.ui" line="242"/>
+        <source>Add contact</source>
+        <translation>Добавить контакт</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="247"/>
+        <source>About program</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="252"/>
+        <source>About Qt</source>
+        <translation>О Qt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="101"/>
+        <source>Invalid Session</source>
+        <translation>Ошибка сессии</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="140"/>
+        <source>Welcome to Chat!</source>
+        <translation>Добро пожаловать в чат!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="150"/>
+        <location filename="mainwindow.cpp" line="207"/>
+        <source>Clear</source>
+        <translation>Очистить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="155"/>
+        <source>New Contact</source>
+        <translation>Новый контакт</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="155"/>
+        <source>Do you want to add new contact: </source>
+        <translation>Вы хотите добавить новый котакт:</translation>
+    </message>
+</context>
+<context>
+    <name>Options</name>
+    <message>
+        <location filename="options.ui" line="14"/>
+        <source>Options</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="options.ui" line="26"/>
+        <source>Set design</source>
+        <translation>Установить дизайн</translation>
+    </message>
+    <message>
+        <location filename="options.ui" line="49"/>
+        <source>LogOut</source>
+        <oldsource>Unlogin</oldsource>
+        <translation>Выти из аккаунта</translation>
+    </message>
+    <message>
+        <location filename="options.ui" line="62"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <location filename="options.ui" line="75"/>
+        <source>Set laguage</source>
+        <translation>Установить язык</translation>
+    </message>
+    <message>
+        <location filename="options.ui" line="89"/>
+        <source>Russian</source>
+        <translation>Русский</translation>
+    </message>
+    <message>
+        <location filename="options.ui" line="94"/>
+        <source>English</source>
+        <translation>Английский</translation>
+    </message>
+</context>
+<context>
+    <name>authwindow</name>
+    <message>
+        <location filename="authwindow.ui" line="26"/>
+        <source> Authorization</source>
+        <translation>Авторизация</translation>
+    </message>
+    <message>
+        <location filename="authwindow.ui" line="53"/>
+        <source>Login</source>
+        <translation>Логин</translation>
+    </message>
+    <message>
+        <location filename="authwindow.ui" line="63"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+    <message>
+        <location filename="authwindow.ui" line="85"/>
+        <source>Enter</source>
+        <translation>Войти</translation>
+    </message>
+    <message>
+        <location filename="authwindow.ui" line="95"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <location filename="authwindow.ui" line="105"/>
+        <source>Register</source>
+        <translation>Регистрация</translation>
+    </message>
+    <message>
+        <location filename="authwindow.ui" line="121"/>
+        <source>Welcome to Chat!</source>
+        <translation>Добро пожаловать в чат!</translation>
+    </message>
+    <message>
+        <location filename="authwindow.cpp" line="25"/>
+        <source>Successeful autorisation!</source>
+        <translation>Успешная авторизация!</translation>
+    </message>
+    <message>
+        <location filename="authwindow.cpp" line="30"/>
+        <source>Not Found!</source>
+        <translation>Не найдено!</translation>
+    </message>
+    <message>
+        <location filename="authwindow.cpp" line="53"/>
+        <source>Successeful registration!</source>
+        <translation>Успешная регистрация!</translation>
+    </message>
+</context>
+</TS>
